1ad7898fcf09d3c5921aa0cdfd06103a767e5ce4
montanpi <12225722+montanpi@users.noreply.github.com>
2023-02-20 19:57:54 +0900

    add event stream

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -1,60 +1,20 @@
 class GameController < ApplicationController
   def index; end
 
-  def update_matrix
-    current_user.matrix = game_of_life(current_user.matrix)
-    current_user.save
-    update_matrix_text
-  end
+  # def update_matrix
+  #   current_user.matrix = game_of_life(current_user.matrix)
+  #   current_user.save
+  #   update_matrix_text
+  # end
 
-  private
+  # private
 
-  def update_matrix_text
-    render turbo_stream:
-      turbo_stream.replace(
-        'matrix',
-        partial: 'game/matrix',
-        locals: { matrix: current_user.matrix }
-      )
-  end
-
-  def game_of_life(input)
-    matrix = input.split("\n").map { |row| row.chars }
-    rows = matrix.length
-    cols = matrix.first.length
-    output = Array.new(rows) { Array.new(cols) }
-    (0...rows).each do |row|
-      (0...cols).each do |col|
-        cell = matrix[row][col]
-        live_neighbors = count_live_neighbors(matrix, row, col)
-        output[row][col] = if cell == '*' && [2, 3].include?(live_neighbors)
-                             '*'
-                           elsif cell == '.' && live_neighbors == 3
-                             '*'
-                           else
-                             '.'
-                           end
-      end
-    end
-    output.map { |row| row.join }.join("\n")
-  end
-
-  def count_live_neighbors(matrix, row, col)
-    rows = matrix.length
-    cols = matrix.first.length
-    offsets = [
-      [-1, -1], [-1, 0], [-1, 1],
-      [0, -1], [0, 1],
-      [1, -1], [1, 0], [1, 1]
-    ]
-    live_count = 0
-    offsets.each do |offset|
-      neighbor_row = row + offset[0]
-      neighbor_col = col + offset[1]
-      next unless neighbor_row >= 0 && neighbor_row < rows && neighbor_col >= 0 && neighbor_col < cols
-
-      live_count += 1 if matrix[neighbor_row][neighbor_col] == '*'
-    end
-    live_count
-  end
+  # def update_matrix_text
+  #   render turbo_stream:
+  #     turbo_stream.replace(
+  #       'matrix',
+  #       partial: 'game/matrix',
+  #       locals: { matrix: current_user.matrix }
+  #     )
+  # end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
 
   get 'game', to: 'game#index', as: 'game'
   get 'game/update_matrix', to: 'game#update_matrix', as: 'update_matrix'
+
+  get 'sse', to: 'sse#start', as: 'start_sse'
+  get 'sse', to: 'sse#stop', as: 'stop_sse'
+
   post 'home/upload'
   root 'home#index'
 end
