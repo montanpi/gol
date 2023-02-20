4fc99410c10039aa3f2ba5e2584690600e309719
Pietro Montanari <pietro.montanari@gmx.it>
2023-02-20 19:57:55 +0900

    add tailwindcss

--- app/javascript/controllers/game_controller.js
+++ app/javascript/controllers/game_controller.js
@@ -4,9 +4,11 @@ export default class extends Controller {
   stream = null;
   connect() {
     document.getElementById("start-sse").disabled = false
+    document.getElementById("start-sse").classList.remove('translate-y-0.5', 'bg-blue-700')
     document.getElementById("start-sse").addEventListener("click", (e) => {
       e.preventDefault();
       document.getElementById("start-sse").disabled = true;
+      document.getElementById("start-sse").classList.add('translate-y-0.5', 'bg-blue-700')
       this.stream = new EventSource("/sse");
       this.stream.addEventListener("update-event", (event) => {
         const { matrix } = JSON.parse(event.data);
@@ -16,6 +18,7 @@ export default class extends Controller {
     document.getElementById("stop-sse").addEventListener("click", (e) => {
       e.preventDefault();
       document.getElementById("start-sse").disabled = false;
+      document.getElementById("start-sse").classList.remove('translate-y-0.5', 'bg-blue-700')
       this.stream.close();
     });
   }
