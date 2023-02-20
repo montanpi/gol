51b190bdd38ed9478b882dbe979addbae3a49474
Pietro Montanari <pietro.montanari@gmx.it>
2023-02-20 19:57:54 +0900

    refactor pages to home controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,7 @@
 Rails.application.routes.draw do
-  get 'game', to: "game#index", as: "game"
-
-  get "game/update_matrix", to: "game#update_matrix", as: "update_matrix"
-
-  root 'pages#home'
   devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get 'game', to: "game#index", as: "game"
+  get "game/update_matrix", to: "game#update_matrix", as: "update_matrix"
+  root 'home#index'
 end
