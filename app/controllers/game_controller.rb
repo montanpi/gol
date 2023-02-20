592515074e9ab9adeabadfaf6209dda52208b0e2
montanpi <12225722+montanpi@users.noreply.github.com>
2023-02-20 19:57:54 +0900

    refactor pages to home controller

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -1,5 +1,4 @@
 class GameController < ApplicationController
-  before_action :authenticate_user!
 
   def index
   end
--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -1,8 +1,8 @@
 require "test_helper"
 
-class PagesControllerTest < ActionDispatch::IntegrationTest
+class HomeControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
-    get pages_home_url
+    get root_url
     assert_response :success
   end
 end
