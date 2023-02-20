f929e8f1cf5d8cf8f27ce7a36b6e5200f3bba6a3
Pietro Montanari <pietro.montanari@gmx.it>
2023-02-20 19:57:53 +0900

    add counter partial and put it behind login

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+
+  get "update_counter", to: "pages#update_counter", as: "update_counter"
+
   root 'pages#home'
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
